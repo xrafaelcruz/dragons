794c4d9b06c41ab133544599d832bc7a550a3018
Rafael Cruz <xrafaelcruz@gmail.com>
2019-08-28 00:22:41 -0300

    Starting the Dragon List

---
+++ src/components/DragonList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import DragonListStyle from 'styles/dragonList';
+
+function DragonList() {
+  return <DragonListStyle></DragonListStyle>;
+}
+
+export default DragonList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import Page from 'styles/page';
+import DragonList from 'components/DragonList';
+
 export default function Home(props) {
-  return <div data-page="home">Home</div>;
+  return (
+    <Page data-page="home">
+      <DragonList />
+    </Page>
+  );
 }
